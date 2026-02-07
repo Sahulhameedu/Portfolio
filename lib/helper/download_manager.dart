fcb75e4a5d074a90c8ce976d5a657f61298f5616
Sahulhameedu <hameedsahul046@gmail.com>
2026-02-08 00:34:02 +0530

    Download Fix

--- lib/helper/download_manager.dart
+++ lib/helper/download_manager.dart
@@ -1,11 +1,20 @@
-import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter/services.dart';
+import 'package:universal_html/html.dart' as html;
 
 class DownloadManager {
-  static Future<void> downloadCV() async {
-    final url = Uri.parse('/assets/cv/Sahulhameed_Resume.pdf');
-    await launchUrl(
-      url,
-      mode: LaunchMode.externalApplication,
-    ).catchError((error) {});
+  static Future downloadCV() async {
+    final byteData = await rootBundle.load('assets/cv/Sahulhameed_Resume.pdf');
+
+    // Convert to blob
+    final blob = html.Blob([byteData.buffer.asUint8List()], 'application/pdf');
+    final url = html.Url.createObjectUrlFromBlob(blob);
+
+    // Trigger download
+    final anchor = html.AnchorElement(href: url)
+      ..download = 'Sahulhameed_Resume.pdf'
+      ..click();
+
+    // Clean up
+    html.Url.revokeObjectUrl(url);
   }
 }
