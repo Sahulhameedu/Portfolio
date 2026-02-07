879af3a9a1d891af91bd9f7953b6db38f9c2272c
Sahulhameedu <hameedsahul046@gmail.com>
2026-02-08 00:52:56 +0530

    debugger added

--- lib/helper/email_sender.dart
+++ lib/helper/email_sender.dart
@@ -3,6 +3,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class EmailSender {
   static Future<void> sendEmail(Map<String, dynamic> data) async {
+    print(dotenv.env);
     final emailSeriveId = dotenv.env['EMAIL_SERVICE_ID']!;
     final emailTemplateId = dotenv.env['EMAIL_TEMPLATE_ID']!;
     await emailjs.send(emailSeriveId, emailTemplateId, data);
