db8dd27110e3517be059b4c8ab570612245590fd
Sahulhameedu <hameedsahul046@gmail.com>
2026-02-07 22:58:00 +0530

    Update .gitignore and configuration files for build and service worker

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -54,7 +54,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "dbdf5de0f678d24583ab03695b90f2bd
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "0e48ba2a435a52e1030731c6f9ed3954",
+"flutter_bootstrap.js": "c953e63c52b04d4371457abf9eabeeec",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
